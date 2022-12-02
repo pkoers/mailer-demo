d4f4f348cfe39f7ecceaa6ed6eb091060ad24b8b
Patrick Koers <pkoers75@gmail.com>
2022-12-02 19:17:32 +0100

    fix to complete welcome test

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,6 +2,6 @@ class UserMailerPreview < ActionMailer::Preview
   def welcome
     user = User.first
     # This is how you pass value to params[:user] inside mailer definition!
-    UserMailer.with(user: user).welcome
+    UserMailer.welcome(user)
   end
 end
